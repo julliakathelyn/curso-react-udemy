3af462e83d70e533befdeb20996057dbcecf3d8b
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2026-03-09 19:24:08 -0300

    Conhecendo o JSX e comentarios

--- 02_FUNDAMENTOS/fundamentos/src/App.js
+++ 02_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
+// exemplo de comentario em JSX
 import './App.css';
 import FirstComponent from './components/FirstComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      {/* Comentário em JSX */}
+      {/* os comentarios em JSX nao vazam para o usuario, ou seja, nao aparecem no console do navegador */}
+      
        <h1>Fundamentos React</h1>
        <FirstComponent />
-      </header>
+       <p>É padrão que os elementos sejam envolvidos por uma div</p>
+       <p>Não podemos usar apenas class, devemos usar className isso vai dar erro no console</p>
+
+      
     </div>
   );
 }
