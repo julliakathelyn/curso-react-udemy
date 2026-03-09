4d982c1cd75a73c3500ea1cc6aee85abaead16cc
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2026-03-09 12:26:55 -0300

    Update title in index.html and modify App.js content for Desafio 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá Mundo</h1>
+      <h1>Desafio 1</h1>
+      <p>1 - Entre no nosso arquivo App.js e adicione mais algum elemento HTML da sua escolha;</p>
+      <p>2 - Crie uma regra de estilos em App.css que altere a cor do seu elemento;</p>
+      <p>3 - Vá até o arquivo index.html e altere o título da aplicação na meta tag.</p>
+
+
     </div>
   );
 }
