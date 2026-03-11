3855dc4cb14797671ad6b2b6f33e159e05c709f4
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2026-03-11 13:00:35 -0300

    maneiras de usar imagens em react

--- 03_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 03_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import City from './assets/city.jpg'
 
 function App() {
   return (
     <div className="App">
       <h1>Desafio 3</h1>
+      {/* Imagem em public */}
+      {/* imagens da pasta public podem ser acessadas diretamente usando a /nomedaimg.blabla */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* imagem em assets*/}
+      {/* imagem em assets precisam ser importadas */}
+      <div>
+        <img src={City} alt="" />
+      </div>
+
     </div>
   );
 }
