3006b72523a1313492392f8ef4fa7b4a94d2578c
julliakathelyn <julliakathelyn07@gmail.com>
2026-04-16 14:50:28 -0300

    simplificando a manipulacao

--- 05_FORMULARIOS/forms/src/components/MyForm.js
+++ 05_FORMULARIOS/forms/src/components/MyForm.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 import './MyForm.css'
 
 const MyForm = () => {
+    const [name, setName ] = useState('')
+    const [email, setEmail ] = useState('')
+
+    const handleName = (e) => {
+        console.log(e.target.value)
+    }
+
+    console.log(name)
+
+
   return (
     <div>
         {/*  1 criacao de form */}
         <form>
             <div>
                 <label htmlFor="name">Nome: </label>
-                <input type="text" id="name" name="name" placeholder="Digite seu nome"/>
+                <input type="text" id="name" name="name" placeholder="Digite seu nome" onChange={handleName}/>
             </div>
             {/* 2 label envolvendo input, esse faz a mesma coisa do que o de cima mas usa menos codigo */}
 
