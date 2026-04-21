5a52d6214a40a7d97bd8a7d1bfce43dc1827c3fe
julliakathelyn <julliakathelyn07@gmail.com>
2026-04-21 17:14:58 -0300

    resetando formularios

--- 05_FORMULARIOS/forms/src/components/MyForm.js
+++ 05_FORMULARIOS/forms/src/components/MyForm.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { useState } from 'react'
 import './MyForm.css'
 
-const MyForm = ({user}) => {
-    const [name, setName ] = useState(user ? user.name : '')
-    const [email, setEmail ] = useState(user)
+const MyForm = ({ user }) => {
+    const [name, setName] = useState(user ? user.name : '')
+    const [email, setEmail] = useState(user)
 
     const handleName = (e) => {
         console.log(e.target.value)
@@ -12,48 +12,55 @@ const MyForm = ({user}) => {
         console.log(email)
     }
 
-  
+
 
     const handleSubmit = (event) => {
         event.preventDefault()
         console.log('Enviando o formulário')
 
+
+        {/* 7 limoando form */ }
+        setName('');
+        setEmail('');
     };
 
-  return (
-    <div>
-        {/*  1 criacao de form */}
-        {/* 5 envio de form */}
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Nome: </label>
-                <input 
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Digite seu nome"
-                onChange={handleName}
-                value={name}
-                />
-            </div>
-            {/* 2 label envolvendo input, esse faz a mesma coisa do que o de cima mas usa menos codigo */}
 
-            <label>
-                <span>Email:</span>
-                {/* 4 Simplificação de manipulação de state */}
-                <input type="email"
-                name="email"
-                placeholder="Digite seu email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                  />
-            </label>
-            <input type="submit" value="Enviar"/>
 
-        </form>
-      
-    </div>
-  )
+    return (
+        <div>
+            {/*  1 criacao de form */}
+            {/* 5 envio de form */}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Nome: </label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Digite seu nome"
+                        onChange={handleName}
+                        value={name}
+                    />
+                </div>
+                {/* 2 label envolvendo input, esse faz a mesma coisa do que o de cima mas usa menos codigo */}
+
+                <label>
+                    <span>Email:</span>
+                    {/* 4 Simplificação de manipulação de state */}
+                    <input type="email"
+                        name="email"
+                        placeholder="Digite seu email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                </label>
+                {/* 8 textarea */}
+                <input type="submit" value="Enviar" />
+
+            </form>
+
+        </div>
+    )
 }
 
 export default MyForm
