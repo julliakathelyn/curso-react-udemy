2aeb0c594dff85e6aff2322806c34c0360350a9d
julliakathelyn <julliakathelyn07@gmail.com>
2026-04-23 08:59:25 -0300

    tela inicial estilos

---
+++ 06_SECRET_WORD/secretword/src/components/StartScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>CLique no botao abaixo para comecar a jogar</p>
+      <button>Começar</button>
+    </div>
+  )
+}
+
+export default StartScreen
